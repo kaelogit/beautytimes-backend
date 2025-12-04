ceed91b960d7e4368278ad0f27bb75635bab7d15
kaelogit <ksqkareem@gmail.com>
2025-12-04 22:06:16 +0100

    secured database

--- beautytime_project/settings.py
+++ beautytime_project/settings.py
@@ -3,6 +3,9 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
+load_dotenv()
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
