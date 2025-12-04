08317c8e743da8151c4b2d4a1779bebe1704a2e6
kaelogit <ksqkareem@gmail.com>
2025-12-04 20:52:06 +0100

    Configure for render deployment with Neon DB

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 # Exit on error
 set -o errexit
 
-# Install dependencies from your fixed requirements file
+# Install dependencies
 pip install -r requirements.txt
 
 # Convert static files
 python manage.py collectstatic --no-input
 
-# Apply database migrations
+# Run migrations (Good practice to keep cloud DB in sync)
 python manage.py migrate
